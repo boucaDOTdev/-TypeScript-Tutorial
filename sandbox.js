6bfeae84f60073df8bfd7c704a619958e9be0390
Alexandre Bouça <alexandre.bouca.99@gmail.com>
2022-03-06 20:05:10 +0000

    TypeScript Tutorial #3 - Type Basics

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,13 @@
-var character = "luigi";
-console.log(character);
-var inputs = document.querySelectorAll("input");
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var character = "mario";
+var age = 22;
+var isBlackBelt = false;
+//character = 20
+character = "luigi";
+//age = "yoshi"
+age = 23;
+//isBlackBelt = "yes"
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
