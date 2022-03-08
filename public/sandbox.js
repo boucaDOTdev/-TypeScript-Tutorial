2943dfc79367ca404ca258aa3370f08be47e66f7
Alexandre Bouça <alexandre.bouca.99@gmail.com>
2022-03-08 02:14:37 +0000

    TypeScript Tutorial #10 - Function Signatures

---
+++ public/sandbox.js
@@ -0,0 +1,22 @@
+"use strict";
+//let greet: Function
+//ex 1
+let greet;
+greet = (name, greeting) => {
+    console.log(`${name} says ${greeting}`);
+};
+//ex 2
+let calc;
+calc = (numOne, numTwo, action) => {
+    if (action === "add") {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
+};
+//ex 3
+let logDetails;
+logDetails = (ninja) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`);
+};
