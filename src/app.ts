1fed44d2a02dd7de4a7bc5b47e282c64e589e774
Alexandre Bouça <alexandre.bouca.99@gmail.com>
2022-03-12 16:09:06 +0000

    TypeScript Tutorial #13 - Public, Private & Readonly

---
+++ src/app.ts
@@ -0,0 +1,45 @@
+//classes
+class Invoice {
+  //readonly client: string
+  //private details: string
+  //amount: number
+
+  constructor(
+    readonly client: string,
+    private details: string,
+    public amount: number
+  ) {}
+
+  format() {
+    //inv.client = "something else"
+    return `${this.client} owes €${this.amount} for ${this.details}`
+  }
+}
+
+const invOne = new Invoice("mario", "work on the mario website", 250)
+const invTwo = new Invoice("luigi", "work on the luigi website", 300)
+
+let invoices: Invoice[] = []
+invoices.push(invOne)
+invoices.push(invTwo)
+
+invoices.forEach((inv) => {
+  //inv.client = "something else"
+  console.log(inv.client, inv.amount, inv.format())
+})
+
+//const form = document.querySelector("form")!
+
+const form = document.querySelector(".new-item-form") as HTMLFormElement
+console.log(form.children)
+
+//inputs
+const type = document.querySelector("#type") as HTMLSelectElement
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement
+const details = document.querySelector("#details") as HTMLInputElement
+const amount = document.querySelector("#amount") as HTMLInputElement
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault()
+  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber)
+})
